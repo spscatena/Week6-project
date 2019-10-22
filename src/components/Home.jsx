772c3ee65edfd88e982810b24730ade088abdd98
Sabrina Scatena <sabrina.scatena@gmail.com>
2019-10-22 13:37:46 -0400

    homepage in good shape

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,30 +1,59 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import imgMinion from '../minion1.png';
-
+import minionHome from '../minionHome.png';
+import pirate from '../pirate-home.png';
+import chefHome from '../chefHome.png';
+import chefHome2 from '../chefHome2.png'
 
 
 export default function Home() {
   return (
-    <div id="home-div">
+    <div id="container">
+      <div id="welcome"><h1>Welcome to a Fun Translation!</h1></div>
+      <div id="home-div">
+        <div id="pirate-home">
+          <Link to="/pirate">
+            <img src={pirate} />
+          </Link>
+        </div>
 
-      <div id="pirate-home">
-        <Link to="/pirate">
-          <img src={imgMinion} />
-        </Link>
+        <div id="chef-home">
+          <Link to="/chef">
+            <img src={chefHome} />
+          </Link>
+        </div>
+
+        <div id="minion-home">
+          <Link to="/minion">
+            <img src={minionHome} />
+          </Link>
+        </div>
       </div>
-
-      <div id="chef-home">
-        <img src="https://vignette.wikia.nocookie.net/muppet/images/c/c9/SC_169.jpg/revision/latest/scale-to-width-down/300?cb=20180407180306" />
-      </div>
-
-
-      <div id="minion-home">
-        <Link to="/minion">
-          <img src="https://cdn.clipart.email/fef41ed77c15fb5fb564446a37e9c2ad_28-collection-of-minion-clipart-no-background-high-quality-_236-406.jpeg" />
-        </Link>
-      </div>
+      <div id="instructions">Click on one of the cards to retreive a fun translation of any text in Pirate speak, the accent of the Sweedish Chef or Minion language.</div>
     </div>
   )
 }
 
+
+
+// export default function Home() {
+//   return (
+//     <div id="home-div">
+//       <div id="pirate-home">
+//         <Link to="/pirate">
+//           <img src={pirate} />
+//         </Link>
+//       </div>
+
+//       <div id="chef-home">
+//         <img src={chefHome} />
+//       </div>
+
+//       <div id="minion-home">
+//         <Link to="/minion">
+//           <img src={minionHome} />
+//         </Link>
+//       </div>
+//     </div>
+//   )
+// }
