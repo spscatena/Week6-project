2850bfa95bfa6f2679cd0db4bafa817b5e992021
Sabrina Scatena <sabrina.scatena@gmail.com>
2019-10-23 14:45:14 -0400

    done

--- src/components/ChefPage.jsx
+++ src/components/ChefPage.jsx
@@ -31,9 +31,9 @@ class ChefPage extends React.Component {
           {this.props.chefMessage !== "" &&
             // <Animated animationIn="tada" animationOut="tada" isVisible={true} animationInDuration="900"> */}
             <div id="chefHome2">
-              <img src={chefHome2} />
-            </div>
-            // </Animated> */}
+                <img src={chefHome2} />
+              </div>
+            // </Animated>
           }
         </div>
       </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,23 +10,25 @@ export default function Home() {
     // <body>
     <div id="container">
       <div id="welcome"><h1>Welcome to a Fun Translation!</h1></div>
-      <div id="home-div">
-        <div id="pirate-home">
-          <Link to="/pirate">
-            <img src={pirate} />
-          </Link>
-        </div>
+      <div className="img-container">
+        <div id="home-div">
+          <div id="pirate-home">
+            <Link to="/pirate">
+              <img className="home-images" src={pirate} />
+            </Link>
+          </div>
 
-        <div id="chef-home">
-          <Link to="/chef">
-            <img src={chefHome} />
-          </Link>
-        </div>
+          <div id="chef-home">
+            <Link to="/chef">
+              <img className="home-images" src={chefHome} />
+            </Link>
+          </div>
 
-        <div id="minion-home">
-          <Link to="/minion">
-            <img src={minionHome} />
-          </Link>
+          <div id="minion-home">
+            <Link to="/minion">
+              <img className="home-images" src={minionHome} />
+            </Link>
+          </div>
         </div>
       </div>
       <div id="instructions">Click on one of the cards to retreive a fun translation of any text in Pirate speak, the accent of the Sweedish Chef or Minion language.</div>
